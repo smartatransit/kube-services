e6c944688975e517fa84223360600dcf3064b2ae
Xander Flood <xander.t.flood@gmail.com>
2020-09-26 23:46:53 -0400

    non-superuser connection

--- postgres.tf
+++ postgres.tf
@@ -1,6 +1,7 @@
 provider "postgresql" {
-  host     = var.postgres_internal_name
-  username = "admin"
-  password = var.postgres_admin_password
-  sslmode  = "disable"
+  host      = var.postgres_internal_name
+  username  = "admin"
+  password  = var.postgres_admin_password
+  sslmode   = "disable"
+  superuser = false
 }
